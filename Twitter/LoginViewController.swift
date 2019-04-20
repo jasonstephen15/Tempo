3818ff5542117aa11ff88898036047719e93b5e0
Jason Stephen <jason@wireless-10-147-197-71.public.utexas.edu>
2019-04-20 16:06:32 -0500

    working auth

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -9,33 +9,13 @@
 import UIKit
 import WebKit
 
-class LoginViewController: UIViewController, WKNavigationDelegate {
+class LoginViewController: UIViewController, WKNavigationDelegate{
 
-    var webView: WKWebView!
-    
-    override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        title = webView.title
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 1
-        let url = URL(string: "")!
-        webView.load(URLRequest(url: url))
-        
-        // 2
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        toolbarItems = [refresh]
-        navigationController?.isToolbarHidden = false
+    
 
-        // Do any additional setup after loading the view.
     }
     
 
@@ -49,22 +29,19 @@ class LoginViewController: UIViewController, WKNavigationDelegate {
     
     @IBAction func onLoginButton(_ sender: Any) {
         
-        let myurl = "https://api.twitter.com/oauth/request_token"
-        UserDefaults.standard.set(true, forKey: "userLoggedIn")
-        
-        TwitterAPICaller.client?.login(url: myurl, success: {
-            self.performSegue(withIdentifier: "loginToHome", sender: self)
-        }, failure: { (Error) in
-            print("no login")
-        })
-        
-        
-        
+//        let myurl = "https://api.twitter.com/oauth/request_token"
+//        UserDefaults.standard.set(true, forKey: "userLoggedIn")
+//
+//        TwitterAPICaller.client?.login(url: myurl, success: {
+//            self.performSegue(withIdentifier: "loginToHome", sender: self)
+//        }, failure: { (Error) in
+//            print("no login")
+//        })
         
         let SpotifyClientID = "1387473f6a5f4ac38e34153b0cb4df83"
         let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
-        
-        var configuration = SPTConfiguration(
+
+         var configuration = SPTConfiguration(
             clientID: SpotifyClientID,
             redirectURL: SpotifyRedirectURL
         )
