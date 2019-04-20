f264e65fd994ff2097d4103a68624eca8aed9deb
shriprama <shriprama@gmail.com>
2019-04-20 14:34:49 -0500

    added bridging header

---
+++ Tempo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Tempo-Bridging-Header.h
+//  
+//
+//  Created by Shriprama Rao on 4/20/19.
+//
+
+#ifndef Tempo_Bridging_Header_h
+#define Tempo_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* Tempo_Bridging_Header_h */
