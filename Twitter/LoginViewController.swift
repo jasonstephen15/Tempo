1bbbbdae4ebea527c6227403e3a5a5a62f730181
Jason Stephen <jason@wireless-10-147-197-71.public.utexas.edu>
2019-04-23 12:10:03 -0500

    working login

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -7,55 +7,60 @@
 //
 
 import UIKit
-import WebKit
 
-class LoginViewController: UIViewController, WKNavigationDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class LoginViewController: UIViewController, SPTSessionManagerDelegate {
+
+    fileprivate let SpotifyClientID = "1387473f6a5f4ac38e34153b0cb4df83"
+    fileprivate let SpotifyRedirectURI = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
+
+    lazy var configuration: SPTConfiguration = {
+        let configuration = SPTConfiguration(clientID: SpotifyClientID, redirectURL: SpotifyRedirectURI)
+        // Set the playURI to a non-nil value so that Spotify plays music after authenticating and App Remote can connect
+        // otherwise another app switch will be required
+        configuration.playURI = ""
         
+        // Set these url's to your backend which contains the secret to exchange for an access token
+        // You can use the provided ruby script spotify_token_swap.rb for testing purposes
+        configuration.tokenSwapURL = URL(string: "https://amethyst-plain.glitch.me/api/token")
+        configuration.tokenRefreshURL = URL(string: "https://amethyst-plain.glitch.me/api/refresh_token")
+        return configuration
+    }()
     
+    lazy var sessionManager: SPTSessionManager = {
+        let manager = SPTSessionManager(configuration: configuration, delegate: self)
+        return manager
+    }()
 
+    
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        print("success", session)
+    }
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        print("fail", error)
+    }
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        print("renewed", session)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
 
     override func viewDidAppear(_ animated: Bool) {
         
-        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
-            self.performSegue(withIdentifier: "loginToHome", sender: self )
-        }
+ 
     }
-    
     
     @IBAction func onLoginButton(_ sender: Any) {
         
-//        let myurl = "https://api.twitter.com/oauth/request_token"
-//        UserDefaults.standard.set(true, forKey: "userLoggedIn")
-//
-//        TwitterAPICaller.client?.login(url: myurl, success: {
-//            self.performSegue(withIdentifier: "loginToHome", sender: self)
-//        }, failure: { (Error) in
-//            print("no login")
-//        })
-        
-        let SpotifyClientID = "1387473f6a5f4ac38e34153b0cb4df83"
-        let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
-
-         var configuration = SPTConfiguration(
-            clientID: SpotifyClientID,
-            redirectURL: SpotifyRedirectURL
-        )
-        
+        let requestedScopes: SPTScope = [.appRemoteControl]
+        self.sessionManager.initiateSession(with: requestedScopes, options: .default)
     }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        self.sessionManager.application(app, open: url, options: options)
+        return true
     }
-    */
-
 }
