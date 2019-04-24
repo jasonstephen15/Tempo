584086469e9ee511a47f61f18f4cc428af938b00
Jason Stephen <jason@wireless-10-145-175-98.public.utexas.edu>
2019-04-24 18:34:48 -0500

    working GET

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -50,7 +50,21 @@ class LoginViewController: UIViewController, UIApplicationDelegate, SPTSessionMa
         token = session.accessToken
         self.appRemote.connect()
         
+        
+        let headers: HTTPHeaders = [
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self.appRemote.connectionParameters.accessToken!
+        ]
+        
+        Alamofire.request("https://api.spotify.com/v1/users/jasonstephen15/playlists", method: .get, headers: headers).responseJSON { response in
+            debugPrint(response)
+        }
+        
+        
         self.performSegue(withIdentifier: "loginToHome", sender: self)
+        
+        
     }
     func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
         print("fail", error)
@@ -95,107 +109,7 @@ class LoginViewController: UIViewController, UIApplicationDelegate, SPTSessionMa
         
         let requestedScopes: SPTScope = [.appRemoteControl]
         self.sessionManager.initiateSession(with: requestedScopes, options: .default)
-        
-        print("Session started")
-        
-//        Alamofire.request("https://api.spotify.com/v1/users/jasonstephen15/playlists")
-//            .response {response in
-//                print(response)
-//
-//        }
-        
-        
-        //ANOTHER TRYYY
-        
-        
-//        fetch("https://api.spotify.com/v1/audio-analysis/6EJiVf7U0p1BBfs0qqeb1f", {
-//            method: "GET",
-//            headers: {
-//                Authorization: `Bearer ${userAccessToken}`
-//            }
-//        })
-//            .then(response => response.json())
-//            .then(({beats})) => {
-//                beats.forEach((beat, index) => {
-//                    console.log(`Beat ${index} starts at ${beat.start}`);
-//                    })
-//        }
-        
-        //ANOTHER TRYYYYy
-//        let string = "https://api.spotify.com/v1/users/jasonstephen15/playlists"
-//        let url = NSURL(string: string)
-//        let request = NSMutableURLRequest(url: url! as URL)
-//
-//        let code = "BQBFuLvjCPwnr3FsIy9CnraG1sTAc"//self.appRemote.connectionParameters.accessToken
-//
-//        request.setValue("Bearer" + code, forHTTPHeaderField: "Authorization")
-//        //request.addValue("clientIDhere", forHTTPHeaderField: "Authorization")
-//        request.httpMethod = "GET"
-//        let session = URLSession.shared
-//
-//        let tache = session.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
-//            if let antwort = response as? HTTPURLResponse {
-//                let code = antwort.statusCode
-//                print(code)
-//            }
-//        }
-//        tache.resume()
-        
-        //ANOTHEEEERRR TRYYYy
-        
 
-        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
-        let parameters = ["id": 13, "name": "jack"] as [String : Any]
-
-        //create the url with URL
-        let url = URL(string: "https://api.spotify.com/v1/users/jasonstephen15/playlists")! //change the url
-
-        //create the session object
-        let session = URLSession.shared
-
-        //now create the URLRequest object using the url object
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET" //set http method as POST
-
-        do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
-        } catch let error {
-            print(error.localizedDescription)
-        }
-
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("Bearer " + token, forHTTPHeaderField: "Authorization")
-
-        //create dataTask using the session object to send data to the server
-        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-
-            guard error == nil else {
-                return
-            }
-
-            guard let data = data else {
-                return
-            }
-
-            do {
-                //create json object from data
-                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                    
-                    print("AWESOME!!!")
-                    print(json)
-                    
-                    
-                    // handle json...
-                }
-            } catch let error {
-                print("ERRORORROROROROORORORORORRRRR")
-                print(error.localizedDescription)
-            }
-        })
-        task.resume()
-        
-        
         
     }
 
